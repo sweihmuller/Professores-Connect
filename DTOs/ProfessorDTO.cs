6341e7acc14bb53b7cae2f2274f16fc64eb2d324
Santi <Santi@DESKTOP-IG9DTLS>
2025-03-04 17:15:02 -0300

    Add project files.

---
+++ DTOs/ProfessorDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Professores_Connect.DTOs
+{
+    public class ProfessorDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }  
+    }
+}
